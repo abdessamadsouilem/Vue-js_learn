ddf4c79b51ba18627d9b7bc29a8a157090abbce8
abdessamad <abdessamadsouilem1@gmail.com>
2021-04-12 14:35:08 +0000

    commit

---
+++ learn_vue/src/components/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, ut velit excepturi sit dignissimos quam adipisci laborum veniam? Libero asperiores molestiae porro accusamus blanditiis alias ratione eveniet laudantium expedita. Ducimus.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
